d1c48b1156448608893471c2f0ccafcba2759106
David Bowers <dbowersinc@hotmail.com>
2023-09-11 17:35:14 -0700

    Refactored session to run script

--- task_tracker/main.py
+++ task_tracker/main.py
@@ -4,25 +4,24 @@ from task_tracker.data.model import User, Task
 
 
 # Handle user
-def handle_user(username: str, password: str):
+def handle_user(session, username: str, password: str):
     print("handle_user()")
     print(f"username: {username}")
     print(f"password: {password}")
     # Verify user credentials
     payload = {}
-    with Session() as session:
-        user = session.scalar(select(User).where(User.username == username))
-        if user:
-            if password == user.password:
-                payload["user"] = user
-                payload["status"] = "User verified."
-                # TODO: log record of user login
-                # TODO: User should handle login
-                # TODO: set logged in flag for user
-        else:
-            payload["user"] = None
-            payload["status"] = "Credentials not found."
-        print(user.password)
+    user = session.scalar(select(User).where(User.username == username))
+    if user:
+        if password == user.password:
+            payload["user"] = user
+            payload["status"] = "User verified."
+            # TODO: log record of user login
+            # TODO: User should handle login
+            # TODO: set logged in flag for user
+    else:
+        payload["user"] = None
+        payload["status"] = "Credentials not found."
+    print(user.password)
 
     return payload
 
