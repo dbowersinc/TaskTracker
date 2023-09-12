c1f0ece0ec22f607e8fded6e045ba43181265fb4
David Bowers <dbowersinc@hotmail.com>
2023-09-12 11:19:02 -0700

    Running loop

--- task_tracker_cli/cli.py
+++ task_tracker_cli/cli.py
@@ -52,39 +52,48 @@ def run(username, password):
     from task_tracker.taskshandler import TasksHandler
     from task_tracker.menus.menu import Menu
     from sqlalchemy import select
+    running = True
 
-    # User connection
-    with Session() as session:
-        response = handle_user(session, username, password)
-        current_user = response["user"]
-        if current_user:
-            click.echo(f"Welcome {current_user.username}!")
-            # User logged in, is there a current task?
-            if current_user.user_current_tasks:
-                click.echo(f"Current task: {current_user.current_task.description}")
-                states_handler = TasksHandler([current_user.current_task], current_user)
-                state_options = Menu("Task State", "Select a state for the current task.",
-                                     states_handler.get_state_options())
-                display_menu(state_options, session)
+
+    while running:
+        # User connection
+        with Session() as session:
+            response = handle_user(session, username, password)
+            current_user = response["user"]
+            if current_user:
+                click.echo(f"Welcome {current_user.username}!")
+                # User logged in, is there a current task?
+                if current_user.user_current_tasks:
+                    click.echo(f"Current task: {current_user.user_current_tasks.description}")
+                    states_handler = TasksHandler([current_user.current_task], current_user)
+                    state_options = Menu("Task State", "Select a state for the current task.",
+                                         states_handler.get_state_options())
+                    display_menu(state_options, session)
+
+                else:
+                    click.echo("No current task. Delivering list of tasks.")
+                    # Get list of tasks marked todo.
+                    todo_tasks = session.scalars(select(Task).where(Task.state == "todo")).all()
+                    tasks_handler = TasksHandler(todo_tasks, current_user)
+                    tasks_menu = Menu("Tasks", "Select a task to work on.", tasks_handler.get_task_options())
+                    display_menu(tasks_menu, session)
+                    # click.echo(f"{tasks_menu.title}\n{tasks_menu.intro}")
+                    # for option in tasks_menu.options_menu:
+                    #     click.echo(f"{option}")
+                    # TODO: Accept string input to enter a new task.
+                    # TODO: A option to enter a new task.
 
             else:
-                click.echo("No current task. Delivering list of tasks.")
-                # Get list of tasks marked todo.
-                todo_tasks = session.scalars(select(Task).where(Task.state == "todo")).all()
-                tasks_handler = TasksHandler(todo_tasks, current_user)
-                tasks_menu = Menu("Tasks", "Select a task to work on.", tasks_handler.get_task_options())
-                display_menu(tasks_menu, session)
-                # click.echo(f"{tasks_menu.title}\n{tasks_menu.intro}")
-                # for option in tasks_menu.options_menu:
-                #     click.echo(f"{option}")
-                # TODO: Accept string input to enter a new task.
-                # TODO: A option to enter a new task.
+                click.echo("User not found.")
+            session.commit()
 
-        else:
-            click.echo("User not found.")
-        session.commit()
+        # publish action report and prompt for another action
 
-    # Actions based on user current tasks
+        # If user selects exit then running is False
+        running = False
+
+        # Save the user and password to environment variables if running is True.
+
 
     # Report and exit || perform another action
     click.echo("Goodbye!")
